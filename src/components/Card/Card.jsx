2ea19c6ae81f2368dac52213841cd6e6360e8536
ChaofanMa <chaofanmah@gmail.com>
2025-01-30 12:14:54 -0500

    main card styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,15 @@
 import "./Card.scss";
 
 function Card({ object, index }) {
+    const changeSymbols = (str) => {
+        const temp = document.createElement("div");
+        temp.innerHTML = str;
+        return temp.textContent;
+      };
+
   return (
     <article className="card">
-      <h2 className="card__question">{object.question}</h2>
+      <h2 className="card__question">{changeSymbols(object.question)}</h2>
       <div>
         <p className="card__answer">{object.correct_answer}</p>
         <p className="card__cat">{object.category}</p>
