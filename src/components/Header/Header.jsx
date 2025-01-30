7ad6724ce9514641ef2f2d989ef4c9ff6b8b27af
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-01-30 11:11:01 -0500

    basic feature and style for header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import "./Header.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import cats from "../../../tags.json";
+
+function Header() {
+  console.log(cats);
+  //   const [cats, setCats] = useState([]);
+
+  //   useEffect(() => {
+  //     const fetchCats = async () => {
+  //       try {
+  //         const { data } = await axios.get(`${API_URL}/cats`);
+  //         setCats(data);
+  //       } catch (error) {
+  //         console.error(error);
+  //       }
+  //     };
+  //     fetchCats();
+  //   }, [API_URL]);
+
+  return (
+    <header className="header">
+      <h1 className="header__title">Trivia</h1>
+      <div>
+        <label className="header__label" htmlFor="category">
+          category
+        </label>
+        <select className="header__select">
+          <option value="">Select a category...</option>
+          {cats.map((cat, index) => (
+            <option key={index} value={cat} className="header__option">
+              {cat}
+            </option>
+          ))}
+        </select>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
